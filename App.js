17b63c9216ad86abd0df8bad5fa38326276e9fea
Suporte TI <ti@palmeiras.com.br>
2021-05-17 12:40:43 -0300

    Tabuleiro com Minas

--- App.js
+++ App.js
@@ -18,42 +18,51 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import params from './src/params';
-import Field from './src/components/Fields';
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions';
 
 export default class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = this.createSatte()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createSatte = () =>{
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return{
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
   render(){
     return(
       <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.welcome}>Tamanho da grade:
         {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-        <Field/>
-        <Field opened/>
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={4} />
-        <Field opened nearMines={7} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
+        <View style={styles.board}>
+          <MineField board={this.state.board}/>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FcFF',
-  },
-    welcome:{
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+   
+},
+  board:{
+   alignItems:'center',
+   backgroundColor: '#aaa' 
+  }
 });
-
-
